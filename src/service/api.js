bdfd1db96424895fbd5bb3b6957f17b4304383a9
guilhermemd <guilherme.md@gmail.com>
2021-10-04 22:04:24 -0300

    feat: :sparkles: initial commit with mock api

---
+++ src/service/api.js
@@ -0,0 +1,67 @@
+import image_1 from '../images/image-1.png';
+import image_2 from '../images/image-2.png';
+import image_3 from '../images/image-3.png';
+
+
+const api = [
+  {
+    id: 1,
+    image: image_1,
+    product: 'MACACÃO FEMININO MARIA FILÓ SOBREPOSIÇÃO',
+    price: 'R$ 759,00',
+    price_x: '12 X R$63,25',
+  },
+  {
+    id: 2,
+    image: image_2,
+    product: 'REGATA FEMININA DE TRICOT COM OMBREIRA',
+    price: 'R$ 759,00',
+    price_x: '12 X R$63,25',
+  },
+  {
+    id: 3,
+    image: image_3,
+    product: 'BLUSA FEMININA SILK MERCI',
+    price: 'R$ 759,00',
+    price_x: '12 X R$63,25',
+  },
+  {
+    id: 4,
+    image: image_1,
+    product: 'BLAZER FEMININA LIOCEL PUNHO',
+    price: 'R$ 759,00',
+    price_x: '12 X R$63,25',
+  },
+  {
+    id: 5,
+    image: image_1,
+    product: 'MACACÃO FEMININO MARIA FILÓ SOBREPOSIÇÃO',
+    price: 'R$ 759,00',
+    price_x: '12 X R$63,25',
+  },
+  {
+    id: 6,
+    image: image_2,
+    product: 'REGATA FEMININA DE TRICOT COM OMBREIRA',
+    price: 'R$ 759,00',
+    price_x: '12 X R$63,25',
+  },
+  {
+    id: 7,
+    image: image_3,
+    product: 'BLUSA FEMININA SILK MERCI',
+    price: 'R$ 759,00',
+    price_x: '12 X R$63,25',
+  },
+  {
+    id: 8,
+    image: image_1,
+    product: 'BLAZER FEMININA LIOCEL PUNHO',
+    price: 'R$ 759,00',
+    price_x: '12 X R$63,25',
+  },
+]
+
+module.exports = {
+  api,
+}
