73d07cb6e2491159ef3b373ceba3ae22442176b9
guilhermemd <guilherme.md@gmail.com>
2021-10-06 16:09:26 -0300

    season img with grid

--- src/components/Season.jsx
+++ src/components/Season.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ImageBackground } from 'react-native';
 import summer from '../images/image-5.png';
 import blanc from '../images/image-6.png';
 import '../style/season.css';
@@ -8,30 +7,28 @@ import '../style/season.css';
 function Season() {
   return (
     <section className="season">
-      <ImageBackground source={ require(summer)} >
-      <div className="season__left__text">
-        <h4>ÍRIS</h4> <h4>VERÃO</h4> <h4>2022</h4>
-      </div>
-      </ImageBackground>  
-      {/* <div className="img_left">
+      <div className="season__left__box" >
         <img 
           src={ summer }
           alt="summer-collection"
-          className="season__img1"
+          className="season__img__left"
         >
-        </img> */}
-      {/* </div> */}
-      <div className="img_right">
+        </img>
+        <div className="season__left__text">
+          <h4>ÍRIS</h4> <h4>VERÃO</h4> <h4>2022</h4>
+        </div> 
+      </div> 
+      <div className="season__right__box">
         <img 
           src={ blanc }
           alt="blanc-collection"
-          className="season__img2"
+          className="season__img__right"
         >
         </img>
-      </div>
         <div className="season__right__text">  
           <h4>LE LIS</h4> <h4>BLANC</h4>
         </div>
+      </div>    
     </section>
   );
 }
